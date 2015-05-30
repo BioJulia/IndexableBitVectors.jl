2ea84b4398b3e51cb5649e21c4a4a4f04f53fdc9
Kenta Sato <bicycle1885@gmail.com>
2015-05-30 20:13:35 +0900

    add a simple succinct bit vector

--- src/IndexableDicts.jl
+++ src/IndexableDicts.jl
@@ -1,9 +1,19 @@
 module IndexableDicts
 
-export rank, rank0, rank1, select, select0, select1, NotFound
+export
+    # types
+    AbstractIndexedBitVector, AbstractBitVector,
+    SuccinctBitVector,
+    # operations
+    rank, rank0, rank1, select, select0, select1
 
-const NotFound = -1
+import Base: rank, select, show, size, push!, getindex, length, endof, convert
 
+abstract AbstractIndexedBitVector <: DenseArray{Bool,1}
+typealias AbstractBitVector Union(BitVector,AbstractIndexedBitVector)
+
+include("common.jl")
 include("bitvector.jl")
+include("succbitvector.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,31 +1,170 @@
 using IndexableDicts
 using FactCheck
 
-facts("rank/select") do
-    context("BitVector") do
-        # BitVector
-        b = convert(BitVector, [0, 0, 1, 1])
-        # rank
-        @fact rank(0, b, 0) => 0
-        @fact rank(0, b, 1) => 1
-        @fact rank(0, b, 2) => 2
-        @fact rank(0, b, 3) => 2
-        @fact rank(0, b, 4) => 2
-        @fact_throws rank(0, b, 5) "out of bound"
-        @fact rank(1, b, 0) => 0
-        @fact rank(1, b, 1) => 0
-        @fact rank(1, b, 2) => 0
-        @fact rank(1, b, 3) => 1
-        @fact rank(1, b, 4) => 2
-        @fact_throws rank(1, b, 5) "out of bound"
-        # select
-        @fact select(0, b, 0) => 0
-        @fact select(0, b, 1) => 1
-        @fact select(0, b, 2) => 2
-        @fact select(0, b, 3) => NotFound
-        @fact select(1, b, 0) => 0
-        @fact select(1, b, 1) => 3
-        @fact select(1, b, 2) => 4
-        @fact select(1, b, 3) => NotFound
+srand(12345)
+
+function test_access{T}(::Type{T})
+    b = convert(T, Int[])
+    @fact_throws b[0]
+    @fact_throws b[1]
+
+    b = convert(T, [1])
+    @fact_throws b[0]
+    @fact b[1] => 1
+    @fact_throws b[2]
+
+    b = convert(T, [0])
+    @fact_throws b[0]
+    @fact b[1] => 0
+    @fact_throws b[2]
+
+    b = convert(T, [0, 0, 1, 1])
+    @fact_throws b[0]
+    @fact b[1] => 0
+    @fact b[2] => 0
+    @fact b[3] => 1
+    @fact b[4] => 1
+    @fact_throws b[5]
+
+    b = convert(T, trues(1024))
+    @fact b[1] => true
+    @fact b[end] => true
+    @fact all([b[i] for i in 1:1024]) => true
+
+    b = convert(T, falses(1024))
+    @fact b[1] => false
+    @fact b[end] => false
+    @fact all([b[i] for i in 1:1024]) => false
+
+    bitv = rand(1024) .> 0.5
+    b = convert(T, bitv)
+    for i in 1:1024; @fact b[i] => bitv[i]; end
+end
+
+function test_rank{T}(::Type{T})
+    b = convert(T, Int[])
+    @fact rank0(b, 0) => 0
+    @fact_throws rank0(b, 1)
+    @fact rank1(b, 0) => 0
+    @fact_throws rank1(b, 1)
+
+    b = convert(T, [0])
+    @fact rank0(b, 0) => 0
+    @fact rank0(b, 1) => 1
+    @fact_throws rank0(b, 2)
+    @fact rank1(b, 0) => 0
+    @fact rank1(b, 1) => 0
+    @fact_throws rank1(b, 2)
+
+    b = convert(T, [1])
+    @fact rank0(b, 0) => 0
+    @fact rank0(b, 1) => 0
+    @fact_throws rank0(b, 2)
+    @fact rank1(b, 0) => 0
+    @fact rank1(b, 1) => 1
+    @fact_throws rank1(b, 2)
+
+    b = convert(T, [0, 0, 1, 1])
+    # rank0
+    @fact rank0(b, 0) => 0
+    @fact rank0(b, 1) => 1
+    @fact rank0(b, 2) => 2
+    @fact rank0(b, 3) => 2
+    @fact rank0(b, 4) => 2
+    @fact rank(0, b, 0) => 0
+    @fact rank(0, b, 4) => 2
+    @fact_throws rank(0, b, 5) "out of bound"
+    # rank1
+    @fact rank1(b, 0) => 0
+    @fact rank1(b, 1) => 0
+    @fact rank1(b, 2) => 0
+    @fact rank1(b, 3) => 1
+    @fact rank1(b, 4) => 2
+    @fact rank(1, b, 0) => 0
+    @fact rank(1, b, 4) => 2
+    @fact_throws rank(1, b, 5) "out of bound"
+
+    b = convert(T, trues(1024))
+    for i in 1:1024; @fact rank0(b, i) => 0; end
+    for i in 1:1024; @fact rank1(b, i) => i; end
+
+    b = convert(T, falses(1024))
+    for i in 1:1024; @fact rank0(b, i) => i; end
+    for i in 1:1024; @fact rank1(b, i) => 0; end
+
+    bitv = rand(1024) .> 0.5
+    b = convert(T, bitv)
+    for i in 1:1024; @fact rank0(b, i) => rank0(bitv, i); end
+    for i in 1:1024; @fact rank1(b, i) => rank1(bitv, i); end
+end
+
+function test_select{T}(::Type{T})
+    b = convert(T, Int[])
+    @fact select0(b, 0) => 0
+    @fact select1(b, 0) => 0
+
+    b = convert(T, [0])
+    @fact select0(b, 0) => 0
+    @fact select0(b, 1) => 1
+    @fact select0(b, 2) => 0
+    @fact select1(b, 0) => 0
+    @fact select1(b, 1) => 0
+    @fact select1(b, 2) => 0
+
+    b = convert(T, [1])
+    @fact select0(b, 0) => 0
+    @fact select0(b, 1) => 0
+    @fact select0(b, 2) => 0
+    @fact select1(b, 0) => 0
+    @fact select1(b, 1) => 1
+    @fact select1(b, 2) => 0
+
+    b = convert(T, [0, 0, 1, 1])
+    # select0
+    @fact select0(b, 0) => 0
+    @fact select0(b, 1) => 1
+    @fact select0(b, 2) => 2
+    @fact select0(b, 3) => 0
+    # select1
+    @fact select1(b, 0) => 0
+    @fact select1(b, 1) => 3
+    @fact select1(b, 2) => 4
+    @fact select1(b, 3) => 0
+
+    b = convert(T, trues(1024))
+    for i in 1:1024; @fact select0(b, i) => 0; end
+    for i in 1:1024; @fact select1(b, i) => i; end
+
+    b = convert(T, falses(1024))
+    for i in 1:1024; @fact select0(b, i) => i; end
+    for i in 1:1024; @fact select1(b, i) => 0; end
+
+    bitv = rand(1024) .> 0.5
+    b = convert(T, bitv)
+    for i in 1:1024; @fact select0(b, i) => select0(bitv, i); end
+    for i in 1:1024; @fact select1(b, i) => select1(bitv, i); end
+end
+
+facts("BitVector") do
+    context("access") do
+        test_access(BitVector)
+    end
+    context("rank") do
+        test_rank(BitVector)
+    end
+    context("select") do
+        test_select(BitVector)
+    end
+end
+
+facts("SuccinctBitVector") do
+    context("access") do
+        test_access(SuccinctBitVector)
+    end
+    context("rank") do
+        test_rank(SuccinctBitVector)
+    end
+    context("select") do
+        test_select(SuccinctBitVector)
     end
 end
