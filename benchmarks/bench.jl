52d6dd017a51b084da13aa2323ec377e6429f4a5
Kenta Sato <bicycle1885@gmail.com>
2015-08-13 18:34:58 +0900

    update benchmarks

--- benchmarks/bench.jl
+++ benchmarks/bench.jl
@@ -2,41 +2,46 @@ using IndexableBitVectors
 
 srand(12345)
 
+const step = 16
+
 function bench_getindex(bv, n)
+    range = 1:step:endof(bv)
     # warming up
-    for i in 1:endof(bv)
+    for i in range
         bv[i]
     end
     t = @elapsed for _ in 1:n
-        for i in 1:endof(bv)
+        for i in range
             bv[i]
         end
     end
-    return t / (n * length(bv))
+    return t / (n * length(range))
 end
 
 function bench_rank1(bv, n)
-    for i in 1:endof(bv)
+    range = 1:step:endof(bv)
+    for i in range
         rank1(bv, i)
     end
     t = @elapsed for _ in 1:n
-        for i in 1:endof(bv)
+        for i in range
             rank1(bv, i)
         end
     end
-    return t / (n * length(bv))
+    return t / (n * length(range))
 end
 
 function bench_select1(bv, n)
-    for i in 1:endof(bv)
+    range = 1:step:endof(bv)
+    for i in range
         select1(bv, i)
     end
     t = @elapsed for _ in 1:n
-        for i in 1:endof(bv)
+        for i in range
             select1(bv, i)
         end
     end
-    return t / (n * length(bv))
+    return t / (n * length(range))
 end
 
 let
@@ -47,16 +52,18 @@ let
         name == "LargeRRR" ? LargeRRR : error("unknown type name: ", name)
     columns = ["type", "length", "r", "bench_getindex", "bench_rank1", "bench_select1"]
     println(join(columns, '\t'))
-    for r in [0.01, 0.1, 0.5], p in 10:2:24
+    for r in [0.01, 0.1, 0.5], p in 10:2:30
+        #info("$r $p")
         len = 2^p
-        vec = rand(len) .< r
+        vec = rand(Float32, len) .< r
         bv = convert(T, vec)
         print(T, '\t', len, '\t', r, '\t')
+        n = max(1, div(2^26, 2^p))
         gc()
-        print(bench_getindex(bv, 5), '\t')
+        print(bench_getindex(bv, n), '\t')
         gc()
-        print(bench_rank1(bv, 5), '\t')
+        print(bench_rank1(bv, n), '\t')
         gc()
-        print(bench_select1(bv, 5), '\n')
+        print(bench_select1(bv, n), '\n')
     end
 end
