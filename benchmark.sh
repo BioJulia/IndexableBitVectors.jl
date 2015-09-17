d0d96b9695b0fd6faaf72c68244eb41fa8f5555c
Kenta Sato <bicycle1885@gmail.com>
2015-09-17 21:00:54 +0900

    remove benchmark.sh

--- benchmark.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin sh
-
-if [[ ! -d benchmarks ]]; then
-    mkdir benchmarks
-fi
-
-julia bench.jl -r -s 0.50 > benchmarks/dense.txt
-julia bench.jl -r -s 0.10 > benchmarks/sparse.txt
-julia bench.jl -r -s 0.01 > benchmarks/very-sparse.txt
